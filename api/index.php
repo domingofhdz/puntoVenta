e75d596dc84aac5d7aa9165b9759f06fec19f637
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2026-09-18 20:14:21 -0500

    Serie de App Web Punto Venta.

---
+++ api/index.php
@@ -0,0 +1,165 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Allow: GET, POST, OPTIONS");
+ 
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    http_response_code(200);
+    exit;
+}
+
+require "conexion.php";
+
+$con = new Conexion(array(
+    "tipo" => "sqlite",
+    "bd"   => "../data/bd.db"
+));
+
+$con->query("CREATE TABLE IF NOT EXISTS productos (
+    id TEXT PRIMARY KEY,
+    nombreProducto TEXT NOT NULL,
+    precio REAL NOT NULL,
+    categoria TEXT NOT NULL
+)");
+
+
+
+require "../firebase-php-jwt/vendor/autoload.php";
+
+$jwtClave = "Test12345-----------------------------------------------";
+
+$headers = getallheaders();
+
+$token = "";
+if (isset($headers["Authorization"])) {
+    $token = str_replace("Bearer ", "", $headers["Authorization"]);
+}
+ 
+try {
+    # el segundo parametro es la clave para codificar y decodificar el JWT
+    # debe ser una string no corta, por eso rellené de guiones
+    $decoded = Firebase\JWT\JWT::decode($token, new Firebase\JWT\Key($jwtClave, "HS256"));
+
+    # $usuario puede ser usada para validaciones
+    $usuario = explode("/", $decoded->sub);
+    $id      = $usuario[0];
+    $usr     = $usuario[1];
+    $nivel   = $usuario[2];
+
+    # $login puede ser usada para validaciones
+    $login = true;
+}
+catch (Exception $error) {
+    $usuario = array();
+    $login   = false;
+}
+ 
+# endpoint para revisar estado de la sesión
+if (isset($_GET["sesion"])) {
+    header("Content-Type: application/json");
+    echo json_encode($usuario);
+}
+# endpoint para iniciar sesión
+elseif (isset($_GET["iniciarSesion"])) {
+    $usuarios = array();
+
+    $nombreUsuario = $_POST["txtUsuario"];
+    $contrasena    = $_POST["txtContrasena"];
+
+    $nombreUsuario = addslashes($nombreUsuario);
+    $contrasena = addslashes($contrasena);
+
+    $sql = "SELECT id, nivelUsuario FROM usuarios
+    WHERE nombreUsuario = '$nombreUsuario' AND contrasena = '$contrasena';";
+
+    foreach ($con->query($sql) as $usuario) {
+        $usuarios[] = $usuario;
+    }
+
+    if (count($usuarios)) {
+        $usuario = $usuarios[0];
+
+        $payload = [
+            "iat" => time(),
+            "exp" => time() + (60 * 60 * 24 * 7),
+            "sub" => $usuario["id"] . "/" . $nombreUsuario . "/" . $usuario["nivelUsuario"]
+        ];
+        # el segundo parametro es la clave para codificar y decodificar el JWT
+        # debe ser una string no corta, por eso rellené de guiones
+        $jwt = Firebase\JWT\JWT::encode($payload, $jwtClave, "HS256");
+
+        echo $jwt;
+    }
+    else {
+        echo "error";
+    }
+}
+
+
+
+if (isset($_GET["buscarProductos"])) {
+    $busqueda = $_GET["txtBusqueda"];
+    $busqueda = addslashes($busqueda);
+
+    $array = array();
+
+    $sql = "SELECT * FROM productos
+    WHERE nombreProducto LIKE '%$busqueda%';";
+
+    foreach ($con->query($sql) as $producto) {
+        $array[] = $producto;
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode($array);
+    exit;
+}
+elseif (isset($_GET["guardarProducto"])) {
+    $id = $_POST["txtId"];
+    $nombreProducto = $_POST["txtNombreProducto"];
+    $precio = $_POST["txtPrecio"];
+    $categoria = $_POST["cboCategoria"];
+
+    if ($id) {
+        $guardar = $con->update("productos");
+        $guardar->set("nombreProducto", $nombreProducto);
+        $guardar->set("precio", $precio);
+        $guardar->set("categoria", $categoria);
+        $guardar->where("id", "=", $id);
+    }
+    else {
+        $guardar = $con->insert("productos", "id, nombreProducto, precio, categoria");
+        $guardar->value(uniqid());
+        $guardar->value($nombreProducto);
+        $guardar->value($precio);
+        $guardar->value($categoria);
+    }
+
+    $guardar->execute();
+}
+elseif (isset($_GET["editarProducto"])) {
+    $id = $_GET["txtId"];
+    $id = addslashes($id);
+
+    $array = array();
+
+    $sql = "SELECT * FROM productos
+    WHERE id = '$id';";
+
+    foreach ($con->query($sql) as $producto) {
+        $array[] = $producto;
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode($array);
+    exit;
+}
+elseif (isset($_GET["eliminarProducto"])) {
+    $id = $_POST["txtId"];
+
+    $delete = $con->delete("productos");
+    $delete->where("id", "=", $id);
+    $delete->execute();
+}
---
+++ app.js
@@ -0,0 +1,178 @@
+function debounce(fun, delay) {
+    let timer
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(function () {
+            fun.apply(this, args)
+        }, delay)
+    }
+}
+
+const app = angular.module("angularjsApp", ["ngRoute"])
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix("")
+
+    $routeProvider
+    .when("/", {
+        templateUrl: "views/login.html",
+        controller: "loginCtrl"
+    })
+    .when("/productos", {
+        templateUrl: "views/productos.html",
+        controller: "productosCtrl"
+    })
+    .otherwise({
+        redirectTo: "/"
+    })
+})
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+    $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
+        const path      = current.$$route.originalPath
+        $rootScope.path = path
+
+        $.extend($.validator.messages, {
+            required: "Llena este campo",
+            number: "Solo números",
+            digits: "Solo números enteros",
+            min: $.validator.format("No valores menores a {0}"),
+            max: $.validator.format("No valores mayores a {0}"),
+            minlength: $.validator.format("Mínimo {0} caracteres"),
+            maxlength: $.validator.format("Máximo {0} caracteres"),
+            rangelength: $.validator.format("Solo {0} caracteres"),
+            equalTo: "El texto de este campo no coincide con el anterior",
+            date: "Ingresa fechas validas",
+            email: "Ingresa un correo electrónico valido"
+        })
+
+        $.ajaxSetup({
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("jwt")}`
+            }
+        })
+    })
+    $rootScope.$on("$routeChangeError", function () {
+    })
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+    })
+}])
+
+app.controller("loginCtrl", function ($scope) {
+    const api = "http://localhost/test/pwas/app2/api"
+
+    $("#frmLogin")
+    .off()
+    .submit(function (event) {
+        event.preventDefault()
+    
+        // Endpoint combinado con la API para iniciar sesión
+        $.post(`${api}/?iniciarSesion`, $(this).serialize(), function (respuesta) {
+            if (respuesta == "error") {
+                alert("Usuario y/o contraseña incorrecto(s)")
+                return
+            }
+
+            // Guarda el JWT en un almacenamiento persistente
+            localStorage.setItem("jwt", respuesta)
+            // Cambia el redireccionamiento según tu aplicación web
+            window.location = "#/productos"
+        })
+    })
+})
+
+app.controller("productosCtrl", function ($scope, $timeout) {
+    function buscar(busqueda) {
+        $.get(`${api}/?buscarProductos`, {
+            txtBusqueda: busqueda || ""
+        }, function (productos) {
+            $timeout(function () {
+                $scope.productos = productos
+            })
+        })
+    }
+
+    const api = "http://localhost/test/pwas/app2/api"
+
+    $scope.productos = []
+
+    buscar()
+
+    $("#frmProducto")
+    .off()
+    .submit(function (event) {
+        event.preventDefault()
+
+        $("#frmProducto").valid()
+
+        if (!validatorProducto.valid()) {
+            return
+        }
+
+        $.post(`${api}/?guardarProducto`, $(this).serialize(), function (respuesta) {
+            $("#frmProducto").get(0).reset()
+            buscar()
+        })
+    })
+    .on("reset", function (event) {
+        // $(':hidden').val("")
+    })
+
+    const validatorProducto = $("#frmProducto").validate({
+        errorClass: "is-invalid",
+        validClass: "is-valid",
+        rules: {
+            txtNombreProducto: {required: true},
+            txtPrecio: {required: true, number: true, min: 0},
+            cboCategoria: {required: true}
+        },
+
+        errorPlacement: function(error, element) {
+            if (element.get(0).type == "password") {
+                error.insertAfter(element.get(0).parentNode)
+            }
+            else {
+                error.insertAfter(element)
+            }
+        }
+    })
+
+    const buscarConDebounce = debounce(function () {
+        buscar($("#txtBusqueda").val())
+    }, 500)
+
+    $("#txtBusqueda")
+    .off()
+    .on("input", buscarConDebounce)
+
+    $(document)
+    .off("click", ".btn-editar")
+    .on("click", ".btn-editar", function (event) {
+        const id = $(this).data("id")
+
+        $.get(`${api}/?editarProducto`, {
+            txtId: id
+        }, function (productos) {
+            const producto = productos[0]
+
+            $("#txtId").val(producto.id)
+            $("#txtNombreProducto").val(producto.nombreProducto)
+            $("#txtPrecio").val(producto.precio)
+            $("#cboCategoria").val(producto.categoria)
+        })
+    })
+
+    $(document)
+    .off("click", ".btn-eliminar")
+    .on("click", ".btn-eliminar", function (event) {
+        const id = $(this).data("id")
+
+        if (!confirm("Quieres eliminar este registro?")) {
+            return
+        }
+
+        $.post(`${api}/?eliminarProducto`, {
+            txtId: id
+        }, function (respuesta) {
+            buscar()
+        })
+    })
+})
