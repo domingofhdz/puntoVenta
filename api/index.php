736c3e260fd0f87bbf76694e0f3c65157cf59f43
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2026-09-19 16:42:42 -0500

    Fix de bug para mandar JWT headers de peticiones.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Allow: GET, POST, OPTIONS");
  
